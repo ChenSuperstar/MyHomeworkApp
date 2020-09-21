39ff405da4af0c0d4fd5c23669273a350b82b4dd
ChenSuperstar <714514530@qq.com>
2020-09-21 15:47:29 +0800

    ScoreBoard app

--- app/src/main/java/com/example/myhomeworkapp/MainActivity.java
+++ app/src/main/java/com/example/myhomeworkapp/MainActivity.java
@@ -9,33 +9,58 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //声明控件对象
-    private EditText input;
-    private TextView output;
-    private Button btn;
-    private String cd;
-    private String fd;
+    private TextView score;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button resetBtn;
+    int nowScore;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input = (EditText)findViewById(R.id.input);
-        output = (TextView)findViewById(R.id.output);
-        btn = (Button)findViewById(R.id.btn);
+        score = (TextView)findViewById(R.id.score);
+        btn1 = (Button)findViewById((R.id.btn1));
+        btn2 = (Button)findViewById((R.id.btn2));
+        btn3 = (Button)findViewById((R.id.btn3));
+        resetBtn = (Button)findViewById((R.id.resetBtn));
 
-        cd = input.getText().toString();
-        double temp = (Double.parseDouble(cd) - 32)/1.8;
-        fd = String.valueOf(temp);
+        nowScore = Integer.parseInt(score.getText().toString());
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                output.setText(fd);
+                nowScore = nowScore+1;
+                score.setText(String.valueOf(nowScore));
             }
         });
 
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nowScore = nowScore+2;
+                score.setText(String.valueOf(nowScore));
+            }
+        });
 
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nowScore = nowScore+3;
+                score.setText(String.valueOf(nowScore));
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nowScore = 0;
+                score.setText(String.valueOf(nowScore));
+            }
+        });
 
 
     }
