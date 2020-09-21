5d3d2a8de35a31badd00b51e14e0d1e4ad999eb5
ChenSuperstar <714514530@qq.com>
2020-09-21 15:05:46 +0800

    20200921 correct some problems, but flash back still exist.

--- app/src/main/java/com/example/myhomeworkapp/MainActivity.java
+++ app/src/main/java/com/example/myhomeworkapp/MainActivity.java
@@ -10,10 +10,11 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     //声明控件对象
-    EditText input;
-    TextView output;
-    String cd;
-    String fd;
+    private EditText input;
+    private TextView output;
+    private Button btn;
+    private String cd;
+    private String fd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +22,21 @@ public class MainActivity extends AppCompatActivity {
 
         input = (EditText)findViewById(R.id.input);
         output = (TextView)findViewById(R.id.output);
-
-        Button btn = (Button)findViewById(R.id.btn);
-        btn.setOnClickListener((View.OnClickListener) this);
+        btn = (Button)findViewById(R.id.btn);
 
         cd = input.getText().toString();
         double temp = (Double.parseDouble(cd) - 32)/1.8;
         fd = String.valueOf(temp);
 
-        output.setText(fd);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                output.setText(fd);
+            }
+        });
+
+
+
 
     }
 }
