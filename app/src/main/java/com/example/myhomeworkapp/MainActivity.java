3843281211b7dac8780e2d8956542b4d698bf9e7
ChenSuperstar <714514530@qq.com>
2020-09-28 22:23:59 +0800

    finish exchange rate version2.0, using sharedPreferences to save data in xxx.xml

--- app/src/main/java/com/example/myhomeworkapp/MainActivity.java
+++ app/src/main/java/com/example/myhomeworkapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myhomeworkapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +15,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
+
     private static final String TAG = "111";
     private EditText input;
     private Button R2DBtn;
@@ -20,19 +24,23 @@ public class MainActivity extends AppCompatActivity {
     private Button R2WBtn;
     private Button configBtn;
     private TextView output;
-    private double rmbAcc;
-    private double R2DER = 0.1472;
-    private double R2EER = 0.1251;
-    private double R2WER = 171.4626;
-    private double result;
-
-
-
+    private float rmbAcc;
+    private float R2DER = 0.14f;
+    private float R2EER = 0.12f;
+    private float R2WER = 171.46f;
+    private float result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = getSharedPreferences("rate", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat("dollar_rate", 0.1f);
+        editor.putFloat("euro_rate", 0.1f);
+        editor.putFloat("won_rate",0.2f);
+        editor.apply();
 
         input = (EditText)findViewById(R.id.input);
         output = (TextView)findViewById(R.id.output);
@@ -41,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         R2WBtn = (Button)findViewById(R.id.R2WBtn);
         configBtn = (Button)findViewById(R.id.config_btn);
 
-        //重置汇率
+
 
 
         R2DBtn.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +60,9 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Please input some number", Toast.LENGTH_SHORT).show();
 
                 }else {
-                    rmbAcc = Double.parseDouble(input.getText().toString());
+                    rmbAcc = Float.parseFloat(input.getText().toString());
+
+                    R2DER = sharedPreferences.getFloat("dollar_rate",R2DER);
                     result = rmbAcc * R2DER;
                     output.setText(String.valueOf(result));
                 }
@@ -64,10 +74,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if("".equals(input.getText().toString())){
 
-                   Toast.makeText(MainActivity.this, "Please input some number", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Please input some number", Toast.LENGTH_SHORT).show();
 
                 }else {
-                    rmbAcc = Double.parseDouble(input.getText().toString());
+                    rmbAcc = Float.parseFloat(input.getText().toString());
+
+                    R2EER = sharedPreferences.getFloat("euro_rate", R2EER);
                     result = rmbAcc * R2EER;
                     output.setText(String.valueOf(result));
                 }
@@ -82,7 +94,9 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Please input some number", Toast.LENGTH_SHORT).show();
 
                 }else {
-                    rmbAcc = Double.parseDouble(input.getText().toString());
+                    rmbAcc = Float.parseFloat(input.getText().toString());
+
+                    R2WER = sharedPreferences.getFloat("won_rate", R2WER);
                     result = rmbAcc * R2WER;
                     output.setText(String.valueOf(result));
                 }
@@ -93,31 +107,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent config = new Intent(MainActivity.this, MainActivity2.class);
-               //这里的是什么
-                config.putExtra("R2Drate",R2DER);
-                config.putExtra("R2Erate",R2EER);
-                config.putExtra("R2Wrate",R2WER);
-                startActivityForResult(config,1);
+                startActivity(config);
             }
         });
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
-        if(requestCode == 1){
-            if(resultCode == RESULT_OK){
-                R2DER = intent.getDoubleExtra("R2Drate",R2DER);
-                R2EER = intent.getDoubleExtra("R2Erate",R2EER);
-                R2WER = intent.getDoubleExtra("R2Wrate",R2WER);
-
-                Log.i(TAG, "now RMB to Dollar Exchange Rate is :" + R2DER );
-                Log.i(TAG, "now RMB to EUR Exchange Rate is :" + R2EER );
-                Log.i(TAG, "now RMB to WON Exchange Rate is :" + R2WER );
-
-            }
-        }
-        super.onActivityResult(requestCode,resultCode,intent);
-
-    }
 }
--- app/src/main/java/com/example/myhomeworkapp/MainActivity2.java
+++ app/src/main/java/com/example/myhomeworkapp/MainActivity2.java
@@ -1,7 +1,9 @@
 package com.example.myhomeworkapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SearchRecentSuggestionsProvider;
+import android.content.SharedPreferences;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
 
     private static final String TAG = "MainActivity2";
     private EditText R2D;
@@ -25,16 +29,16 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Intent intent = getIntent();
-        double dollarRate = intent.getDoubleExtra("R2Drate",0.0f);
-        double euroRate = intent.getDoubleExtra("R2Erate",0.0f);
-        double wonRate = intent.getDoubleExtra("R2Wrate",0.0f);
+        sharedPreferences = getSharedPreferences("rate", Activity.MODE_PRIVATE);
+        final float dollarRate = sharedPreferences.getFloat("dollar_rate", 0.0f);
+        final float euroRate = sharedPreferences.getFloat("euro_rate", 0.0f);
+        final float wonRate = sharedPreferences.getFloat("won_rate", 0.0f);
         R2D = (EditText)findViewById(R.id.R2DErate);
         R2E = (EditText)findViewById(R.id.R2EErate);
         R2W = (EditText)findViewById(R.id.R2WErate);
-        R2D.setHint(dollarRate+"");
-        R2E.setHint(euroRate+"");
-        R2W.setHint(wonRate+"");
+        R2D.setText(dollarRate+"");
+        R2E.setText(euroRate+"");
+        R2W.setText(wonRate+"");
 
         saveBtn = (Button)findViewById(R.id.saveBtn);
 
@@ -44,29 +48,27 @@ public class MainActivity2 extends AppCompatActivity {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    Intent main = new Intent(MainActivity2.this, MainActivity.class);
-                    R2D = findViewById(R.id.R2DErate);
-                    R2E = findViewById(R.id.R2EErate);
-                    R2W = findViewById(R.id.R2WErate);
+                Intent main = new Intent(MainActivity2.this, MainActivity.class);
+                R2D = findViewById(R.id.R2DErate);
+                R2E = findViewById(R.id.R2EErate);
+                R2W = findViewById(R.id.R2WErate);
 
-                    double rate1 = Double.parseDouble(R2D.getText().toString());
-                    double rate2 = Double.parseDouble(R2E.getText().toString());
-                    double rate3 = Double.parseDouble(R2W.getText().toString());
+                float rate1 = Float.parseFloat(R2D.getText().toString());
+                float rate2 = Float.parseFloat(R2E.getText().toString());
+                float rate3 = Float.parseFloat(R2W.getText().toString());
 
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putFloat("dollar_rate", rate1);
+                editor.putFloat("euro_rate", rate2);
+                editor.putFloat("won_rate", rate3);
+                editor.apply();
 
-                    main.putExtra("R2Drate", rate1);
-                    main.putExtra("R2Erate", rate2);
-                    main.putExtra("R2Wrate", rate3);
+                Log.i(TAG, "original RMB to Dollar Exchange Rate is :" + R2D );
+                Log.i(TAG, "original RMB to EUR Exchange Rate is :" + R2E );
+                Log.i(TAG, "original RMB to WON Exchange Rate is :" + R2W );
 
-                    //可以使用Bundle打包传输数据
-
-
-                    Log.i(TAG, "original RMB to Dollar Exchange Rate is :" + R2D );
-                    Log.i(TAG, "original RMB to EUR Exchange Rate is :" + R2E );
-                    Log.i(TAG, "original RMB to WON Exchange Rate is :" + R2W );
-                    setResult(RESULT_OK, main);
-                    finish();
-                }
+                finish();
+            }
 
         });
     }
